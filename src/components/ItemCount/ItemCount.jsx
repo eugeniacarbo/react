9c9d914015a4416d8e4aafc5b8c27bda7581ef84
eugeniacarbo <ecarbo16@gmail.com>
2022-07-13 15:12:15 -0300

    Add files via upload

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import React from 'react'
+
+const ItemCount = ( {initial=1, stock=1, onAdd} ) => {
+
+    const [count, setCount] = useState(1)
+
+    const handdleMas = () => {
+      if (count < stock) {
+        setCount(count + 1)
+      }else {
+        console.log("No hay mas disponible")
+        Toastify({
+          text: "No mas promos",
+          duration: 3000,
+          close: true,
+          gravity: "top", 
+          position: "right", 
+          stopOnFocus: true, 
+          style: {
+            background: "#C84630",
+          },
+        }).showToast();
+      }
+
+    };
+    const handdleMenos = () => {
+      if (count > 1) {
+        setCount(count - 1)
+      }
+    }
+
+    const agregarCarrito = () => {
+      Toastify({
+        text: "Agregado Al Carrito.",
+        duration: 3000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+          background: "#22B14C",
+        },
+      }).showToast();
+    }
+
+  
+  return (
+    <div className="center">
+    <div className="contenedorContador">
+        <div>
+          <img src="https://picsum.photos/id/301/600" alt="" />
+        </div>
+        <div>
+          <h3>Producto Prueba</h3>
+        </div>
+        <div className="container d-flex flex-row justify-content-center">
+          <button className="btn btn-primary btn-sm" onClick={handdleMenos}>-</button>
+          <input type="text" value={count} className="count" />
+          <button className="btn btn-primary btn-sm" onClick={handdleMas}>+</button>
+        </div>
+        <div className="container">
+          <button className="btn btn-success mt-4" onClick={agregarCarrito}>Agregar al carrito</button>
+        </div>
+      </div>
+  </div>
+  )
+}
+
+export default ItemCount
