8a86e49f55f6c52bad16b5fe7d51d9d6374b0c26
eugeniacarbo <ecarbo16@gmail.com>
2022-07-13 14:55:51 -0300

    Add files via upload

---
+++ src/components/CartWidget/Cartwidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cartwidget = () => {
+  return (
+      <a className='carrito'  href="">
+        <img className='iconoCart' src="https://picsum.photos/id/301/600" alt="" />
+        <p>1</p>
+      </a>   
+  )
+}
+
+export default Cartwidget
