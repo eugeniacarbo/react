eabfa8523a266c57f11373f7f252fdef8d602f0d
eugeniacarbo <ecarbo16@gmail.com>
2022-07-18 10:26:29 -0300

    Update ItemListContainer.jsx

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+
+
+
+
+const ItemListContainer = (obj) => {
+  const onAdd = (cantidad) => {
+    console.log(` La cantidad es: ${cantidad}`)
+  }
+  return (
+    <div>
+        <h1> {obj.titulo} </h1>
+        <h3> {obj.subtitulo} </h3>
+        <h4> Contador </h4>
+        <ItemCount initial={1} stock={10} onAdd={onAdd}></ItemCount>
+    </div>
+  )
+}
+
+export default ItemListContainer
