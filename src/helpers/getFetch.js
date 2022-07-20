bbbfa8ad3eb7e5b7a7534467603c6c63e6a3b6ce
eugeniacarbo <ecarbo16@gmail.com>
2022-07-20 15:24:59 -0300

    Add files via upload

---
+++ src/helpers/getFetch.js
@@ -0,0 +1,43 @@
+let productos = [{
+        id: "1",
+        marca: "El Refugio 1",
+        categoría: "Promos",
+        nombre: "Promo finde fuera de temporada",
+        img: ""
+    },
+    {
+        id: "2",
+        marca: "El Refugio 2",
+        categoría: "Promos",
+        nombre: "Promo finde con 6 amigos",
+        img: ""
+    },
+    {
+        id: "3",
+        marca: "El Refugio 2",
+        categoría: "Promos",
+        nombre: "Promo fiestas",
+        img: ""
+    },
+    {
+        id: "4",
+        marca: "El Refugio 1",
+        categoría: "Promos",
+        nombre: "Promo relax finde largo",
+        img: ""
+    },
+    {
+        id: "5",
+        marca: "El Refugio 1",
+        categoría: "Promos",
+        nombre: "Promo solari",
+        img: ""
+    },
+    {
+        id: "6",
+        marca: "El Refugio 1",
+        categoría: "Promos",
+        nombre: "Promo en pareja",
+        img: ""
+    },
+]
