e76b961e63a9dfe0a60fe55c54ea8dcbb32c36f0
eugeniacarbo <ecarbo16@gmail.com>
2022-07-26 11:31:47 -0300

    Update Cartwidget.jsx

--- src/components/CartWidget/Cartwidget.jsx
+++ src/components/CartWidget/Cartwidget.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Cartwidget = () => {
   return (
-      <a className='carrito'  href="">
+      <a className='carrito'  href="/cart">
         <img className='iconoCart' src="https://picsum.photos/id/301/600" alt="" />
-        <p>1</p>
+        
       </a>   
   )
 }
