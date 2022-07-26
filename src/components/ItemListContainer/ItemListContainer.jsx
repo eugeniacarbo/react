8774c193d60a7c063e267fdba1d5863c9bc99c75
eugeniacarbo <ecarbo16@gmail.com>
2022-07-26 11:38:07 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getFetch } from '../../helpers/getFetch'
 import ItemCount from '../ItemCount/ItemCount'
 import ItemList from '../Items/ItemList'
 
-
 const ItemListContainer = () => {
- const [productos,setProductos] = useState([])
+  const [ productos, setProductos ] = useState([])  
+  const [ loading, setLoading ] = useState(true)
 
-const[loading,setLoading] = useState(true)
+  const {categoriaId} = useParams()
 
  useEffect(()=>{
- getFetch()
- .then(respuesta=>(setProductos(respuesta)))
- .catch(err=>(console.log(err)) )
- .finally(()=>setLoading(false))
- },[])
+    if(categoriaId){
+        getFetch() 
+        .then(respuesta => setProductos(respuesta.filter(producto => producto.categoria === categoriaId)))    
+        .catch( err => console.log(err) )
+        .finally(()=> setLoading(false) )
+    }else{
+        getFetch()
+        .then(respuesta => setProductos(respuesta))    
+        .catch( err => console.log(err) )
+        .finally(()=> setLoading(false) )
+    }
+  }, [categoriaId])
+
 
  const onAdd = (cant)=>{
   console.log(`la cantidad es: ${cant}`)
   }
  console.log(productos)
   
+
   return (
     <div>
-        <ItemList></ItemList>
+
+      {loading ?
+      <div className='spinner container'></div>
+      :
+      <ItemList productos={productos}/>
+      }
+
+
     </div>
   )
 }
