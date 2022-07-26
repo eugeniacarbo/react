31bfd075a62a078cdf9caca55eac3a8c7b5c3f6d
eugeniacarbo <ecarbo16@gmail.com>
2022-07-26 11:54:06 -0300

    Update NavBar.jsx

--- src/components/nav.Bar/NavBar.jsx
+++ src/components/nav.Bar/NavBar.jsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react'
 import Cartwidget from '../CartWidget/Cartwidget'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMicrochip} from '@fortawesome/free-solid-svg-icons'
-import {faMemory} from '@fortawesome/free-solid-svg-icons'
-import {faDesktop} from '@fortawesome/free-solid-svg-icons'
-import {faKeyboard} from '@fortawesome/free-solid-svg-icons'
+import {faDesktop, faKeyboard, faMemory, faMicrochip} from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
@@ -15,45 +14,48 @@ const NavBar = () => {
         <div className='barraRGB'></div>
         <nav className="navbar navbar-expand-xl navbar-custom">
             <div className="container-fluid">
-                <a className="navbar-brand ms-4" id='containerLogo' href="#"><img src="https://picsum.photos/id/301/600" id='logo' alt="LogoMarca" /></a>
+                <NavLink to='/'>
+                <span className="navbar-brand ms-4" id='containerLogo'><img src="../../../assets/images/losGamaLogo.svg" id='logoGU' alt="LogoMarca" /></span>
+                </NavLink>
+               
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
+    
                 <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav m-auto mb-2 mb-lg-0 navLinks">
                     <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promociones</a>
+                     <NavLink className='nav-link' to='/categoria/promociones'>Tromociones</NavLink>
+                    
                         <img className='iconos' src="https://picsum.photos/id/301/600" alt="" />
                     </li>
                     <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promociones con amigos</a>
+                     <NavLink className='nav-link' to='/categoria/amigos'>Vacaciones con amigos</NavLink>
+                   
                     <span className='iconosFa'><FontAwesomeIcon icon={faMicrochip}/></span>
                     </li>
                     <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promociones en familia</a>
+                     <NavLink className='nav-link' to='/categoria/familia'>Promo en familia</NavLink>
+                   
                     <img className='iconos' src="https://picsum.photos/id/390/600" alt="" />
                     </li>
                     <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promociones en pareja</a>
+                     <NavLink className='nav-link' to='/categoria/Pareja'>Promociones en pareja</NavLink>
+                    
                     <img className='iconos' src="https://picsum.photos/id/407/600" alt="" />
                     </li>
                     <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promociones solos</a>
+                     <NavLink className='nav-link' to='/categoria/solo'>Vacaciones solo</NavLink>
+                    
                     <img className='iconos' src="https://picsum.photos/id/449/600" alt="" />
-                    </li>
-                    <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Memoria Ram</a>
-                    <span className='iconosFa'><FontAwesomeIcon icon={faMemory}/></span>
-                    </li>
-                    <li className="nav-item me-2">
-                    <a className="nav-link" href="#">Promo finde largo</a>
-                    <img className='iconos' src="https://picsum.photos/id/447/600" alt="" />
                     </li>
                     
                 </ul>
                 </div>
-                <div className='me-4'>
-                    <Cartwidget></Cartwidget>
+                  <div className='me-4'>
+                    <Link to='/cart'>
+                    <Cartwidget/>
+                    </Link>
                 </div>
             </div>
 
