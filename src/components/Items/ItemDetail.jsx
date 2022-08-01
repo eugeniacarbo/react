330271c638cdfc0a2794ea1473a8ed6e161d5ee6
eugeniacarbo <ecarbo16@gmail.com>
2022-08-01 14:24:19 -0300

    Update ItemDetail.jsx

--- src/components/Items/ItemDetail.jsx
+++ src/components/Items/ItemDetail.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({producto}) => {
-  return (
+
+    const [toCart, setTocart] = useState(true)
+
+    const onAdd = (cant) => {
+        console.log(`La cantidad es:  ${cant}`)
+        setTocart(false)
+      }
+      
+
+return (
     <div className='d-flex justify-content-around'>
         <div>
             <img className='mt-5' src={producto.img} alt="" />
@@ -14,9 +23,17 @@ const ItemDetail = ({producto}) => {
             <div className='card-body row'>
                 <h5 className='card-title fs-1 mt-5'>{producto.nombre}</h5>
                 <h4>Precio: {producto.precio} $</h4>
-                <ItemCount></ItemCount>
-            </div>
-            <div className='card-footer text-muted'>
+                  {toCart? 
+                    <ItemCount initial={1} stock={producto.stock} onAdd={onAdd}></ItemCount>
+                    :
+                    <Link to="/Cart"
+                    <button className="btn btn-outline-dark btn-block">
+                    Ir al carrito.
+                    </button>
+                    <br />
+                    </Link>     
+                }   
+                <div className='card-footer text-muted'>
                 stock: {producto.stock}
             </div>
         </div>
