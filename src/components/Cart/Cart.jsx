f52e272b58bb76a99b9236bad2b9c2fa57443243
eugeniacarbo <ecarbo16@gmail.com>
2022-08-01 14:13:10 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>Cart
+     <div>
+        <img src="http://www.fsconcreto.com/" alt="building" />
+        <h1>Sitio en construcción.</h1>
+      </div>
+    </div>
   )
 }
 
