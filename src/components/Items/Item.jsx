4c567252b2f451001e0a0f2ea6aebbedc20130f9
eugeniacarbo <ecarbo16@gmail.com>
2022-08-01 14:21:52 -0300

    Update Item.jsx

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -4,28 +4,49 @@ import ItemCount from '../ItemCount/ItemCount'
 
 
 const Item = ({ prod }) => {
+
+    const [toCart, setTocart] = useState(true)
+
+    const onAdd = (cant) => {
+        console.log(`La cantidad es:  ${cant}`)
+        setTocart(false)
+      }
+
   return (
       <div            
           className='col-md-4 p-1'
-     </div>
-                                                               
-         <div className="card w-100 mt-5" >
+          // key={prod.id}                                                           
+      >                    
+          <div className="card w-100 mt-5" >
               <div className="card-body">
                   <img src={prod.img} alt='' className='w-50' />                                                        
               </div>
               <div className="card-footer">
                 <div className='m-5'>
                 <h3 className='productoTitulo'>{prod.nombre}</h3>
+                <h4 className='productoStock'>Stock disponible: {prod.stock}</h4>
                 </div>
                 <div className='m-3'>
                   <Link to={`/detail/${prod.id}`}>
                         <button className="btn btn-outline-primary btn-block">
-                            Detalle del producto.
+                            detalle del producto
                         </button> 
                   </Link>
                 </div>    
                     <div className='m-5'>
-                    <ItemCount></ItemCount>
+                    {toCart? 
+                    <ItemCount initial={1} stock={prod.stock} onAdd={onAdd}></ItemCount>
+                    :
+                    <Link to="/Cart">
+                        {/* <div>
+                            <h1> Usted ha seleccionado sus proximas vacaciones</h1>
+                        </div> */}
+                    <button className="btn btn-outline-dark btn-block">
+                    Ir al carrito.
+                    </button>
+                    <br />
+                    </Link>     
+                } 
                     </div>
               </div>
           </div>                                                                                                                            
