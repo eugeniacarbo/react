ca13f11189fa3de6dc3e85e056f4e6e14231d7d7
eugeniacarbo <ecarbo16@gmail.com>
2022-08-19 13:34:31 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { firebaseConnectiosns } from './firebase/config'
+import { getFirebaseApp } from './firebase/config'
+
+getFirebaseApp()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <BrowserRouter>
