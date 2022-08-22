6c6b91f46b646a8fd8e800e80d49379a0b038683
eugeniacarbo <ecarbo16@gmail.com>
2022-08-22 14:43:10 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCN04mBb5NDdRAT9YAm8_0ibf8NHJdsqW8",
-  authDomain: "elrefugio-82e61.firebaseapp.com",
-  projectId: "elrefugio-82e61",
-  storageBucket: "elrefugio-82e61.appspot.com",
-  messagingSenderId: "718251816594",
-  appId: "1:718251816594:web:31f66c49d6493555da984b"
+    apiKey: "AIzaSyCN04mBb5NDdRAT9YAm8_0ibf8NHJdsqW8",
+    authDomain: "elrefugio-82e61.firebaseapp.com",
+    projectId: "elrefugio-82e61",
+    storageBucket: "elrefugio-82e61.appspot.com",
+    messagingSenderId: "718251816594",
+    appId: "1:718251816594:web:31f66c49d6493555da984b"
 };
 
 // Initialize Firebase
