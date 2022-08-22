1b0175e774a6ecfba17d9b1e7680a822603f6f5d
eugeniacarbo <ecarbo16@gmail.com>
2022-08-22 14:41:12 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { collection, getDocs, doc, getDoc, getFirestore} from 'firebase/Firebase'
 import { getFetch } from '../../helpers/getFetch'
 import ItemCount from '../ItemCount/ItemCount'
 import ItemList from '../Items/ItemList'
+
+useEffect(() =>{
+  const db = getFirestore() 
+  const queryProducto = doc (db, 'productos', '0VETbnqV6Cz89Sf9Ih3I')
+  getDoc(queryProducto)
+  .then(resp=> setProducto({id: resp.id, ...resp.data() } ) )
+}, [])
+
+console.log(producto)
+
+useEffect(() =>{
+  const db = getFirestore() 
+  const queryCollection = collection (db, 'productos' )
+  getDocs(queryCollection)
+  .then(resp=> setProductos(resp.docs.map(prod => ({id: prod.id, ...prod.data() }) )) )
+  .catch (err => console.log (err))
+  .finally(() => setLoading(false))
+}, [])
+
+console.log(productos)
 
 const ItemListContainer = () => {
   const [ productos, setProductos ] = useState([])  
