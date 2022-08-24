6dad5ec0ea54594e8ed4e43f9e8848a2b5d52180
eugeniacarbo <ecarbo16@gmail.com>
2022-08-24 11:17:24 -0300

    Update ItemDetail.jsx

--- src/components/Items/ItemDetail.jsx
+++ src/components/Items/ItemDetail.jsx
@@ -3,6 +3,8 @@ import ItemCount from '../ItemCount/ItemCount'
 import { useState } from 'react'
 import { useCartContext } from '../../Context/CartContext'
 
+
+
 const ItemDetail = ({producto}) => {
 
     const {agregarCarrito} = useCartContext()
@@ -10,45 +12,51 @@ const ItemDetail = ({producto}) => {
     const [toCart, setTocart] = useState(true)
 
     const onAdd = (cant) => {
+        
         console.log(`La cantidad es:  ${cant}`)
         agregarCarrito({...producto, cantidad: cant})
         setTocart(false)
       }
 
-
-return (
+  return (
     <div className='d-flex justify-content-around'>
         <div>
             <img className='mt-5' src={producto.img} alt="" />
         </div>
-        <div className='card text-center mt-5'>
+        <div className='card text-center mt-5 w-50'>
             <div className='card-header'>
                 <h1 className='fs-6'>{producto.categoria}</h1>
             </div>
             <div className='card-body row'>
                 <h5 className='card-title fs-1 mt-5'>{producto.nombre}</h5>
-                <h4>Precio: {producto.precio} $</h4>
+                <h3 className='fs-4 m-1 text-body'>{producto.descripcion}</h3>
+                <h4 className='fs5 fw-normal'>Precio: {producto.precio} $</h4>
                   {toCart? 
                     <ItemCount initial={1} stock={producto.stock} onAdd={onAdd}></ItemCount>
                     :
-                      <Link to="/Cart">
-                        {/* <div>
-                            <h1> Usted ha seleccionado productos</h1>
-                        </div> */}
+                    <div>
+                    <Link to="/Cart">
                     <button className="btn btn-outline-dark btn-block">
                     Ir al carrito.
                     </button>
                     <br />
-                    </Link>     
+                    </Link>
+                    <Link to="/">
+                    <button className="btn btn-outline-primary btn-block mt-5">
+                        Seguir Comprando
+                    </button>
+                    </Link>
+                    </div>     
                 }   
-              
-                </div>
-                <div className='card-footer text-muted'>
+                
+            </div>
+            <div className='card-footer text-muted'>
                 stock: {producto.stock}
             </div>
         </div>
     </div>
   )
+  
 }
 
 export default ItemDetail
