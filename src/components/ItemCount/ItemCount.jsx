a6a1c7f44eafe88834ba72d7f7e080950baa6114
eugeniacarbo <ecarbo16@gmail.com>
2022-08-28 10:19:41 -0300

    Update ItemCount.jsx

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,41 +1,39 @@
 import { useState } from "react";
 
+const ItemCount = ({ initial = 1, stock = 10, onAdd }) => {
 
-const ItemCount = ( {initial=1, stock=1, onAdd} ) => {
+  const [count, setCount] = useState(1)
 
-    const [count, setCount] = useState(1)
-
-    const handdleMas = () => {
-      if (count < stock) {
-        setCount(count + 1)
-      }else {
-        console.log("No hay mas disponible")
-        Toastify({
-          text: "No hay mas promos",
-          duration: 3000,
-          close: true,
-          gravity: "top", 
-          position: "right", 
-          stopOnFocus: true, 
-          style: {
-            background: "#C84630",
-          },
-        }).showToast();
-      }
-
-    };
-    const handdleMenos = () => {
-    if (count > initial) {
-      setCount(count - 1)
-      }
+  const handdleMas = () => {
+    if (count < stock) {
+      setCount(count + 1)
+    } else {
+      Toastify({
+        text: "No hay stock.",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "#C84630",
+        },
+      }).showToast();
     }
 
-    const agregarCarrito = () => {
+  };
+  const handdleMenos = () => {
+    if (count > initial) {
+      setCount(count - 1)
+    }
+  }
+
+  const agregarCarrito = () => {
     if (count < stock) {
       onAdd(count)
     }
     Toastify({
-      text: "Producto Agregado Al Carrito.",
+      text: "Agregado Al Carrito Tu Reserva.",
       duration: 3000,
       close: true,
       gravity: "top",
@@ -47,17 +45,17 @@ const ItemCount = ( {initial=1, stock=1, onAdd} ) => {
     }).showToast();
   }
 
-  
+
   return (
     <div className="center">
-              <div className="container d-flex flex-row justify-content-center">
-          <button className="btn btn-primary btn-sm" onClick={handdleMenos}>-</button>
-          <input type="text" value={count} className="count" readOnly />
-          <button className="btn btn-primary btn-sm" onClick={handdleMas}>+</button>
-        </div>
-        <div className="container">
-          <button className="btn btn-success mt-4" onClick={agregarCarrito} >Agregar al carrito</button>
-        </div>
+      <div className="container d-flex flex-row justify-content-center">
+        <button className="btn btn-primary btn-sm" onClick={handdleMenos}>-</button>
+        <input type="text" value={count} className="count" readOnly />
+        <button className="btn btn-primary btn-sm" onClick={handdleMas}>+</button>
+      </div>
+      <div className="container">
+        <button className="btn btn-success mt-4" onClick={agregarCarrito} >Agregar al carrito</button>
+      </div>
     </div>
   )
 }
