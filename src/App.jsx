90319539ccf0eb4d4468d473872fccc82deaf537
eugeniacarbo <ecarbo16@gmail.com>
2022-08-28 11:46:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import ItemDetailContainer from './components/Items/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
 import NotFound from './components/NotFound/NotFound'
 import CartContextProvider from './Context/CartContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
-  return (
-     <BrowserRouter>
-        <CartContextProvider>
-    <div className="App">
-      <NavBar>
-         <Routes>
+  return (     
+    <BrowserRouter>
+
+    <CartContextProvider>
+      <div className="App">
+        <NavBar/>
+        <Routes>
           <Route path='/' element={ <ItemListContainer/>} />
           <Route path='/categoria/:categoriaId' element={ <ItemListContainer/>} />
           <Route path='/detail/:detailId' element={ <ItemDetailContainer/> } />
@@ -21,19 +24,16 @@ function App() {
           <Route path='/NotFound' element={ <NotFound/> }/>
 
           <Route path='*' element={ <Navigate to='/NotFound'/> }/>
-            
         </Routes>
-      
-      <div className='prueba' >
-        <br />
-        <br />    
-        <p>Bienvenido a tus próximas vacaciones.  © </p>
-        <br />
+        <div className='prueba' >
+          <br />
+          <br />
+          <p>María Eugenia Carbó  ©</p><a className='iconoGitHub' href="https://github.com/eugeniacarbo/react"><FontAwesomeIcon icon={faGithub}/></a>
+          <br />
+        </div>
       </div>
-    </div>
- </CartContextProvider>
-    </BrowserRouter>  
-
+      </CartContextProvider>
+    </BrowserRouter>
   )
 }
 
