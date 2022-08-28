0ebc740255c095e6e97950980873cee21f279c1d
eugeniacarbo <ecarbo16@gmail.com>
2022-08-28 10:24:40 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,67 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { collection, getDocs, doc, getDoc, getFirestore} from 'firebase/Firebase'
-import { getFetch } from '../../helpers/getFetch'
-import ItemCount from '../ItemCount/ItemCount'
+import { collection, doc, getDoc, getDocs, getFirestore, query, where} from 'firebase/firestore'
 import ItemList from '../Items/ItemList'
-
-useEffect(() =>{
-  const db = getFirestore() 
-  const queryProducto = doc (db, 'productos', '0VETbnqV6Cz89Sf9Ih3I')
-  getDoc(queryProducto)
-  .then(resp=> setProducto({id: resp.id, ...resp.data() } ) )
-}, [])
-
-console.log(producto)
-
-useEffect(() =>{
-  const db = getFirestore() 
-  const queryCollection = collection (db, 'productos' )
-  getDocs(queryCollection)
-  .then(resp=> setProductos(resp.docs.map(prod => ({id: prod.id, ...prod.data() }) )) )
-  .catch (err => console.log (err))
-  .finally(() => setLoading(false))
-}, [])
-
-console.log(productos)
 
 const ItemListContainer = () => {
   const [ productos, setProductos ] = useState([])  
   const [ loading, setLoading ] = useState(true)
-
   const {categoriaId} = useParams()
-
- useEffect(()=>{
-    if(categoriaId){
-        getFetch() 
-        .then(respuesta => setProductos(respuesta.filter(producto => producto.categoria === categoriaId)))    
-        .catch( err => console.log(err) )
-        .finally(()=> setLoading(false) )
-    }else{
-        getFetch()
-        .then(respuesta => setProductos(respuesta))    
-        .catch( err => console.log(err) )
-        .finally(()=> setLoading(false) )
-    }
-  }, [categoriaId])
-
   
+  useEffect(()=>{
+    const db = getFirestore()
+    const queryCollection = collection(db, 'items')
+    const queryCollectionFilter = categoriaId ? query(queryCollection, where('categoria', '==', categoriaId)) : queryCollection
+    getDocs(queryCollectionFilter)
+    .then(resp =>  setProductos( resp.docs.map(prod => ({ id: prod.id, ...prod.data() }) ) ) )
+    .catch( err => console.log(err) )
+    .finally(() => setLoading(false))            
+}, [categoriaId])
 
   return (
     <div>
-
       {loading ?
       <div className='spinner container'></div>
       :
-      <ItemList productos={productos}/>
+      <ItemList productos={productos}/>  
       }
-
-
     </div>
   )
+
+  
 }
 
 export default ItemListContainer
-
-
-
