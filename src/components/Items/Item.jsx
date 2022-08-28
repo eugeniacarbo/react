92122c5f43ccbc16fab8cc5c011c19ba0abc97bb
eugeniacarbo <ecarbo16@gmail.com>
2022-08-28 10:33:21 -0300

    Update Item.jsx

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom'
 import { useCartContext } from '../../Context/CartContext'
 import ItemCount from '../ItemCount/ItemCount'
 
-
 const Item = ({ prod }) => {
-    
+
+
     const {agregarCarrito} = useCartContext()
     const [toCart, setTocart] = useState(true)
 
-   const onAdd = (cant) => {
+    
+    const onAdd = (cant) => {
         agregarCarrito({...prod, cantidad: cant})
         setTocart(false)
       }
-   
+
   return (
-      <div            
-          className='col-md-4 p-1'
-          // key={prod.id}                                                           
-      >                    
+      <div className='col-md-4 p-1'>                    
           <div className="card w-100 mt-5" >
               <div className="card-body">
-                  <img src={prod.img} alt='' className='w-50' />                                                        
+                  <img src={prod.img} alt='' className='w-50'/>                                                        
               </div>
               <div className="card-footer">
                 <div className='m-5'>
@@ -32,7 +30,7 @@ const Item = ({ prod }) => {
                 <div className='m-3'>
                   <Link to={`/detail/${prod.id}`}>
                         <button className="btn btn-outline-primary btn-block">
-                            detalle del producto
+                            Detalle del producto
                         </button> 
                   </Link>
                 </div>    
@@ -45,7 +43,7 @@ const Item = ({ prod }) => {
                     Ir al carrito.
                     </button>
                     <br />
-                    </Link>     
+                    </Link>   
                 } 
                     </div>
               </div>
